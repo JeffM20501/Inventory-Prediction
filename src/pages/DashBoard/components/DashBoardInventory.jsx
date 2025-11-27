14ba42fcbcd94824f779942182facd1f6612279e
JeffM20501 <kimmuna440@gmail.com>
2025-11-27 21:01:48 +0300

    added a table and styles

--- src/pages/DashBoard/components/DashBoardInventory.jsx
+++ src/pages/DashBoard/components/DashBoardInventory.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import TableRender from './TableRender'
 
 function DashBoardInventory({filteredProducts}) {
-    function createData(name, calories, fat, carbs, protein) {
-        return { name, calories, fat, carbs, protein };
+    function createData(name, sku, stock, demandForecast, alerts) {
+        return { name, sku, stock, demandForecast, alerts };
     }
 
     const rows = filteredProducts.map(pro=>
-        createData(pro.name, pro.sku, pro.stock, pro.demand_forecast)
+        createData(pro.name, pro.sku, pro.stock, pro.demand_forecast, pro.alerts)
     )
   return (
     <>
--- src/pages/DashBoard/components/TableRender.jsx
+++ src/pages/DashBoard/components/TableRender.jsx
@@ -6,31 +6,48 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
+import './css/Table.css'
 function TableRender({rows}) {
-    const renderRows=rows.map((row) => (
-        <TableRow
-        key={row.id}
-        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-        >
-        <TableCell component="th" scope="row">{row.name}</TableCell>
-        <TableCell align="right">{row.sku}</TableCell>
-        <TableCell align="right">{row.stock}</TableCell>
-        <TableCell align="right">{row.demand_forecast}</TableCell>
-        <TableCell align="right">{row.stock}</TableCell>
-        </TableRow>
-    ))
-  return (
-    <section>
+    console.log(rows)
+    const headerStyles={
+        letterSpacing:'2px',
+        fontSize:'1.4rem',
+        fontWeight:650,
+        backgroundColor:'var(--tabsBg)'
+    }
+
+    const renderRows=rows.map((row) =>{
+
+        const alertsStyle={
+            backgroundColor:row.alerts && row.alerts.length > 0 ? 'var(--error)' : 'var(--success)',
+            borderRadius:'15px',
+            color:"white",
+            fontWeight:'bold'
+        }
+        return(
+            <TableRow
+                key={row.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                <TableCell className='tbl-name' component="th" scope="row">{row.name}</TableCell>
+                <TableCell align="center">{row.sku}</TableCell>
+                <TableCell align="center">{row.stock}</TableCell>
+                <TableCell align="center">{row.demandForecast}</TableCell>
+                <TableCell align="center" sx={alertsStyle}
+                >{row.alerts&&row.alerts.length>0?"Low Stock":"Green"}</TableCell>
+            </TableRow>
+    )})
+    return (
+    <section className='dash-table'>
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                 <TableRow>
-                    <TableCell>Product Name</TableCell>
-                    <TableCell align="right">SKU</TableCell>
-                    <TableCell align="right">Stock&nbsp;(g)</TableCell>
-                    <TableCell align="right">Demand Forecast&nbsp;(g)</TableCell>
-                    <TableCell align="right">Risk Status&nbsp;(g)</TableCell>
+                    <TableCell sx={headerStyles}>Product Name</TableCell>
+                    <TableCell sx={headerStyles} align="center">SKU</TableCell>
+                    <TableCell sx={headerStyles} align="center">Stock</TableCell>
+                    <TableCell sx={headerStyles} align="center">Demand Forecast(%)</TableCell>
+                    <TableCell sx={headerStyles} align="center">Risk Status</TableCell>
                 </TableRow>
                 </TableHead>
                 <TableBody>
