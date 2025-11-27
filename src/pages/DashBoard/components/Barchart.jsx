661e003c4b56e174a7f671c316d72feb349fd76f
JeffM20501 <kimmuna440@gmail.com>
2025-11-28 02:00:51 +0300

    added a readme and some info

--- src/pages/DashBoard/components/Barchart.jsx
+++ src/pages/DashBoard/components/Barchart.jsx
@@ -1,32 +1,38 @@
 import React from 'react'
 import { Bar, BarChart, ResponsiveContainer, CartesianGrid, Legend, XAxis, YAxis } from 'recharts'
+import { useMemo } from 'react'
 
 function Barchart({products}) {
-    const chartData = Object.entries(products.reduce((sum, pro)=>{
-        const cat = pro.category||"Uncategorized"
-        sum[cat]=(sum[cat]||0)+(pro.stock||0)
-        return sum
-    },{}))
-    const mappedChartData=chartData.map(([category, totalStock])=>({
-        category,
-        totalStock
-    }))
+    const mappedChartData = useMemo(() => {
+        const chartData = Object.entries(products.reduce((sum, pro)=>{
+            const cat = pro.category||"Uncategorized"
+            sum[cat]=(sum[cat]||0)+(pro.stock||0)
+            return sum
+        },{}))
+        
+        return chartData.map(([category, totalStock])=>({
+            category,
+            totalStock
+        }))
+    }, [products]) // Re-run only when products change
+
     console.log("mapped", mappedChartData)
-  return (
-    <ResponsiveContainer height={600} width='100%'>
-        <BarChart margin={{ top: 20, right: 30, left: 20, bottom: 70 }}  data={mappedChartData}>
-            <CartesianGrid strokeDasharray="5 5"/>
-            <XAxis dataKey="category" />
-            <YAxis/>
-            <Legend/>
-            <Bar 
-                fill='var(--color)'
-                stroke='var(--hover)' 
-                dataKey="totalStock" 
-                name="Total Stock by Category"/>
-        </BarChart>
-    </ResponsiveContainer>
-  )
+    
+    return (
+        <ResponsiveContainer height={600} width='100%'>
+            <BarChart margin={{ top: 20, right: 30, left: 20, bottom: 70 }}  data={mappedChartData}>
+                <CartesianGrid strokeDasharray="5 5"/>
+                <XAxis dataKey="category" />
+                <YAxis/>
+                <Legend/>
+                <Bar 
+                    fill='var(--color)'
+                    stroke='var(--hover)' 
+                    dataKey="totalStock" 
+                    name="Total Stock by Category"/>
+            </BarChart>
+        </ResponsiveContainer>
+    )
 }
 
-export default Barchart
+export default React.memo(Barchart) // memorize the comp
--- src/pages/Forecast/Forecast.jsx
+++ src/pages/Forecast/Forecast.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './Forecast.css'
 
 function Forecast() {
   return (
     <>
-      
+      <h1 className='forecast-header'>Forecast comming soon...</h1>
     </>
   )
 }
